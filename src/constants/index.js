1f1141adb38d55f9f329dd16686fffce26c43540
sergey.vodolazhsky <sergey.vodolazhsky@ab-soft.net>
2023-09-18 00:06:31 -0500

    # TODO app - added to config

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,27 @@
 export const projects = [
   {
+    title: "TODO application",
+    desc: "TODO app based on the Laravel Framework with functionality such as: user roles, CRUD, " +
+        "fulltext search, nested tasks with unlimited nesting level, flash messages, filters,",
+    tags: [
+      {
+        name: "Laravel v8.83",
+        color: "green-600",
+      },
+      {
+        name: "JavaScript",
+        color: "green-600",
+      },
+      {
+        name: "SQL",
+        color: "green-600",
+      },
+    ],
+    img: "todo_app.png",
+    link: "https://todo.vodolaz.site/",
+    code: "https://github.com/vodolserge/todo-app",
+  },
+  {
     title: "Digital Blog",
     desc: "A blog based on the Laravel Framework with an Admin Panel and CKEditor, " +
         "implementing basic CRUD operations and Search functionality.",
